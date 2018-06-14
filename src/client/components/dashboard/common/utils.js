b7fcec14cb0dac14345d6c416fd9b98139cd409d
Brendon Co <brendon@cxagroup.com>
2018-06-14 09:56:59 +0800

    Added CircleCI config

---
+++ src/client/components/dashboard/common/utils.js
@@ -0,0 +1,11 @@
+const readableBytes = (bytes, decimals = 2) => {
+  if (bytes === void 0) return "Bundling...";
+  if (bytes == 0) return "0 Bytes";
+  let k = 1000,
+    dm = decimals || 2,
+    sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"],
+    i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+};
+
+export { readableBytes };
